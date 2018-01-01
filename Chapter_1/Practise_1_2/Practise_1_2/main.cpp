641c7d4ede0643ca81ebff5b91379e01afc62290
yangxiaohei <yangxiaohei321123@163.com>
2018-01-01 21:48:06 +0800

    1.1 ~ 1.2

---
+++ Chapter_1/Practise_1_2/Practise_1_2/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  Practise_1_2
+//
+//  Created by YangHan on 2018/1/1.
+//  Copyright © 2018年 YangHan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    /**
+     *  控制台打印语句 Program ended with exit code: 255
+     */
+    return -1;
+}
