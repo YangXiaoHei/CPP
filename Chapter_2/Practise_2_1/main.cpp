1c66b3995bfff804dd6c4e1505b3a3c37b80b231
yangxiaohei <yangxiaohei321123@163.com>
2018-01-08 21:15:24 +0800

    2.1 ~ 2.2

---
+++ Chapter_2/Practise_2_1/main.cpp
@@ -0,0 +1,27 @@
+//
+//  main.cpp
+//  Practise_2_1
+//
+//  Created by YangHan on 2018/1/8.
+//  Copyright © 2018年 YangHan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    /**
+     *
+        int long long long 和 short 的区别在于所占的比特数
+     
+        在 64 位机器上，int 4 字节 long 8 字节 long long 16 字节 short 2 字节
+     
+        无符号类型的二进制最高位被解释为正权重,表示的数值范围比有符号更大，因为最高位用来表示
+        数值而非符号
+        有符号的二进制最高位被解释为负权重，表示数值范围比无符号小，因为最高位用来表示符号
+     
+        float 占 4 字节 double 占 8 字节
+        float 的阶码有 8 位，尾数 23 位
+        double 阶码有 11 位，尾数 52 位
+     */
+    return 0;
+}
---
+++ Chapter_2/Practise_2_2/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  Practise_2_2
+//
+//  Created by YangHan on 2018/1/8.
+//  Copyright © 2018年 YangHan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    /**
+     *  计算贷款时，利率，本金，和付款应该选用 double 类型
+        因为 double 类型的数值范围更大，并且可以表示小数
+     */
+    return 0;
+}
