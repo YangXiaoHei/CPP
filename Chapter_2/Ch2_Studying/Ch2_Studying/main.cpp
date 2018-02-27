74ce3e43f05058bd2365ef921492a24cf21adcac
yangxiaohei <yangxiaohei321123@163.com>
2018-02-27 13:50:56 +0800

    study

--- Chapter_2/Ch2_Studying/Ch2_Studying/main.cpp
+++ Chapter_2/Ch2_Studying/Ch2_Studying/main.cpp
@@ -71,7 +71,45 @@ void unsignedAndSigned() {
     cout << "u2 - u1  =   " << u2 - u1 << endl;
 }
 
+/**
+ *  转义序列
+ */
+void escapeSequence() {
+    cout << "Hi \x4dO\115!\n";
+    /**
+     *  \x 后跟着 1 或多个 十六进制数字
+        或者 \ 后跟着 1, 2, 3 个八进制数字
+     */
+    cout << "\1234" << endl;
+    // 如果反斜线后面跟着的八进制数字超过 3 个，只有前3个数字与 \ 构成转义序列
+}
+
+void literal() {
+    /**
+     
+        字符和字符串字面量
+     
+     *  u   Unicode 16 字符           char16_t
+        U   Unicode 32 字符           char32_t
+        L   w宽字符                    char_t
+        u8  UTF-8(仅用于字符串字面量)    char
+     
+     
+        整型字面量
+     
+        u or U
+        l or L
+        ll or LL
+     
+        浮点型字面值
+     
+        f or F   float
+        l or L   long double
+     */
+    
+}
+
 int main(int argc, const char * argv[]) {
-    unsignedAndSigned();
+    escapeSequence();
     return 0;
 }
