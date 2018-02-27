0a03f5e1e5001df3484a9ee4d090a4db17948bf7
yangxiaohei <yangxiaohei321123@163.com>
2018-02-27 13:51:10 +0800

    2.5 ~ 2.8

---
+++ Chapter_2/Practise_2_7/Practise_2_7/main.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  Practise_2_7
+//
+//  Created by YangHan on 2018/2/27.
+//  Copyright © 2018年 YangHan. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    
+    cout << "Who goes with F\145rgus?\012";   // 字符串 中间有两个转义字符
+    
+    cout << 3.14e1L << endl; // long double
+    
+    cout << 1024.f << endl; // float
+    
+    cout << 3.14L << endl; // long double
+    
+    return 0;
+}
